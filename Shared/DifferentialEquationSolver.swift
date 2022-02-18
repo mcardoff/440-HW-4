44a0af158f88574b3c7430b4e227e3e5853167ef
Michael Cardiff <mcardiff@hawk.iit.edu>
2022-02-18 16:41:55 -0600

    first commit

---
+++ Shared/DifferentialEquationSolver.swift
@@ -0,0 +1,8 @@
+//
+//  DifferentialEquationSolver.swift
+//  Schrodinger-Solver
+//
+//  Created by Michael Cardiff on 2/18/22.
+//
+
+import Foundation
---
+++ Shared/Schrodinger_SolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Schrodinger_SolverApp.swift
+//  Shared
+//
+//  Created by Michael Cardiff on 2/18/22.
+//
+
+import SwiftUI
+
+@main
+struct Schrodinger_SolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
